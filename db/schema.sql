86ed3b31783a313ed701a5bd7b86d964ca2cad59
LilCalc <noahsavinois@gmail.com>
2022-06-12 07:30:07 -0500

    Initial database

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS nodejs_employee_tracker;
+CREATE DATABASE nodejs_employee_tracker;
+USE nodejs_employee_tracker;
+
+/*Create Table for Department*/
+
+
+/*Create Table for Roles*/
+
+
+/*Create Table for Employees*/
